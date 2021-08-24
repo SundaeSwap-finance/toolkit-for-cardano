5ff36bcdce180d6522c8d2192c6a28d14246bcce
Mark Hansen <x@markthemark.com>
2021-08-24 03:51:02 -0400

    feat(ui): tx queries + types

---
+++ ui/src/components/types.ts
@@ -0,0 +1,28 @@
+export type TAsset = {
+  assetId: string;
+  assetName: string;
+  policyId: string;
+  description?: string;
+  logo?: string;
+  name?: string;
+  ticker?: string;
+  url?: string;
+};
+
+export type TAssetAmount = {
+  asset: TAsset;
+  amount: number;
+};
+
+export type TToken = {
+  asset: TAsset;
+  quantity: string;
+};
+
+export type TUtxo = {
+  address: string;
+  datumHash?: string;
+  index: number;
+  tokens: TToken[];
+  value: string;
+};
