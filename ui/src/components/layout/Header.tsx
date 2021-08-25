cfccbd1c068e493907a31f9687baa4385625c647
Mark Hansen <x@markthemark.com>
2021-08-24 23:46:23 -0400

    style(ui): cute ice cream sundaes

--- ui/src/components/layout/Header.tsx
+++ ui/src/components/layout/Header.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import iceCreamCup from "../../assets/ice-cream-cup.png";
+import sundaeLeft from "../../assets/sundae_left.png";
+import sundaeRight from "../../assets/sundae_right.png";
 
 export const Header = () => {
   return (
     <StyledHeader>
+      <div className="sundae-asset--left" />
       <div>{/* <h1>Cardano Toolkit</h1> */}</div>
+      <div className="sundae-asset--right" />
     </StyledHeader>
   )
 }
 
 const StyledHeader = styled.header`
   position: absolute;
-  top: 20px;
+  top: 0;
   left: 0;
   right: 0;
   text-align: center;
-
-  background-image: url(${iceCreamCup});
-  background-position: center;
-  background-size: contain;
-  background-repeat: no-repeat;
+  display: flex;
+  justify-content: space-between;
+  height: 72px;
 
   div {
     height: 40px;
@@ -37,5 +38,22 @@ const StyledHeader = styled.header`
   @media(max-width: 45em) {
     position: initial;
     margin-top: 24px;
+  }
+
+  .sundae-asset--left, .sundae-asset--right {
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    height: 54px;
+    width: 54px;
+    margin: 20px;
+  }
+  .sundae-asset--left {
+    background-image: url(${sundaeLeft});
+    transform: rotate(-30deg);
+  }
+  .sundae-asset--right {
+    background-image: url(${sundaeRight});
+    transform: rotate(30deg);
   }
 `;
